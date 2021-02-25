ebece82e45c9703a4295a9646c6b2a428e5aaeb3
qiuxianhu <2269667628@qq.com>
2021-02-25 16:05:22 +0800

    1、图片裁剪 2、图片压缩

---
+++ WindowsUI/ImageCompress/ImageOperate.cs
@@ -0,0 +1,69 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+
+namespace ImageCompress
+{
+    public class ImageOperate
+    {
+        /// <summary>
+        /// 等比例缩放图片
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <param name="destHeight"></param>
+        /// <param name="destWidth"></param>
+        /// <returns></returns>
+        public static Bitmap ZoomImage(Bitmap bitmap, int destWidth, int destHeight)
+        {
+            try
+            {
+                Image sourImage = bitmap;
+                int width = 0, height = 0;
+                //按比例缩放           
+                int sourWidth = sourImage.Width;
+                int sourHeight = sourImage.Height;
+                if (sourHeight > destHeight || sourWidth > destWidth)
+                {
+                    if ((sourWidth * destHeight) > (sourHeight * destWidth))
+                    {
+                        width = destWidth;
+                        height = (destWidth * sourHeight) / sourWidth;
+                    }
+                    else
+                    {
+                        height = destHeight;
+                        width = (sourWidth * destHeight) / sourHeight;
+                    }
+                }
+                else
+                {
+                    width = sourWidth;
+                    height = sourHeight;
+                }
+                Bitmap destBitmap = new Bitmap(destWidth, destHeight);
+                Graphics g = Graphics.FromImage(destBitmap);
+                g.Clear(Color.Transparent);
+                //设置画布的描绘质量         
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                //g.DrawImage(sourImage, new Rectangle((destWidth - width) / 2, (destHeight - height) / 2, width, height), 0, 0, sourImage.Width, sourImage.Height, GraphicsUnit.Pixel);
+                g.DrawImage(sourImage, new Rectangle(12, 0, width, height), 0, 0, sourImage.Width, sourImage.Height, GraphicsUnit.Pixel);
+
+                g.Dispose();
+                //设置压缩质量     
+                System.Drawing.Imaging.EncoderParameters encoderParams = new System.Drawing.Imaging.EncoderParameters();
+                long[] quality = new long[1];
+                quality[0] = 100;
+                System.Drawing.Imaging.EncoderParameter encoderParam = new System.Drawing.Imaging.EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+                encoderParams.Param[0] = encoderParam;
+                sourImage.Dispose();
+                return destBitmap;
+            }
+            catch
+            {
+                return bitmap;
+            }
+        }
+    }
+}
---
+++ WindowsUI/ImageCrop/ImageOperate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace ImageCrop
+{
+    public class ImageOperate
+    {
+        /// <summary>
+        /// 获取等比例缩放的图片（高宽不一致时获取最中间部分的图片）
+        /// </summary>
+        public static Image AdjImageToFitSize(Image fromImage, int width, int height)
+        {
+            Bitmap bitmap = new Bitmap(width, height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Point[] destPoints = new Point[] {
+                new Point(0, 0),
+                new Point(width, 0),
+                new Point(0, height)
+            };
+            Rectangle rect = GetImageRectangle(fromImage.Width, fromImage.Height);
+            graphics.DrawImage(fromImage, destPoints, rect, GraphicsUnit.Pixel);
+            Image image = Image.FromHbitmap(bitmap.GetHbitmap());
+            bitmap.Dispose();
+            graphics.Dispose();
+            return image;
+        }
+
+        /// <summary>
+        /// 居中位置获取
+        /// </summary>
+        private static Rectangle GetImageRectangle(int w, int h)
+        {
+            int x = 0;
+            int y = 0;
+
+            if (h > w)
+            {
+                h = w;
+                y = (h - w) / 2;
+            }
+            else
+            {
+                w = h;
+                x = (w - h) / 2;
+            }
+
+            return new Rectangle(x, y, w, h);
+        }
+
+        /// <summary>
+        /// 通过地址加载图片
+        /// </summary>
+        /// <param name="url"></param>
+        public static Image LoadPhotoImage(string url)
+        {
+            try
+            {
+                var request = System.Net.WebRequest.Create(url);
+                var response = request.GetResponse();
+                var stream = response.GetResponseStream();
+                return stream == null ? null : Image.FromStream(stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 绘制圆形头像框
+        /// </summary>
+        public static Region DrawingCircle(Rectangle clientRectangle)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddEllipse(clientRectangle);
+            Region region = new Region(gp);
+            return region;
+        }
+    }
+}
